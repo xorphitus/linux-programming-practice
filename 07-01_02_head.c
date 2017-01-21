2336c703d4c5b69463de636510082b3f7d60a615
xorphitus <xorphitus@gmail.com>
2017-01-22 02:10:33 +0900

    headがファイルを扱えるようにした

--- 07-01_02_head.c
+++ 07-01_02_head.c
@@ -4,11 +4,25 @@
 static void do_head(FILE *f, long nlines);
 
 int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    fprintf(stderr, "Usage: %s n\n", argv[0]);
+  if (argc < 2) {
+    fprintf(stderr, "Usage: %s n [file file...]\n", argv[0]);
     exit(1);
   }
-  do_head(stdin, atol(argv[1]));
+  long nlines = atol(argv[1]);
+  if (argc == 2) {
+    do_head(stdin, nlines);
+  } else {
+    for (int i = 2; i < argc; i++) {
+      FILE *f;
+      f = fopen(argv[i], "r");
+      if (!f) {
+        perror(argv[i]);
+        exit(1);
+      }
+      do_head(f, nlines);
+      fclose(f);
+    }
+  }
   exit(0);
 }
 
