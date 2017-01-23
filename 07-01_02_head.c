37e127694a61cba296b38fec7a6da17b1ffbec96
xorphitus <xorphitus@gmail.com>
2017-01-24 00:24:37 +0900

    headがオプションを扱えるようにした

--- 07-01_02_head.c
+++ 07-01_02_head.c
@@ -1,18 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define _GNU_SOURCE
+#include <getopt.h>
+
 static void do_head(FILE *f, long nlines);
 
+#define DEFAULT_N_LINES 10
+
+static struct option longopts[] = {
+  {"lines", required_argument, NULL, 'n'},
+  {"help",  no_argument,       NULL, 'h'},
+  {0, 0, 0, 0}
+};
+
 int main(int argc, char *argv[]) {
-  if (argc < 2) {
-    fprintf(stderr, "Usage: %s n [file file...]\n", argv[0]);
-    exit(1);
+  long nlines = DEFAULT_N_LINES;
+  int opt;
+  while ((opt = getopt_long(argc, argv, "n:", longopts, NULL)) != -1) {
+    switch (opt) {
+      case 'n':
+        nlines = atoi(optarg);
+        break;
+      case 'h':
+        fprintf(stdout, "Usage: %s [-n LINES] [FILE ...]\n", argv[0]);
+        exit(0);
+      case '?':
+        fprintf(stdout, "Usage: %s [-n LINES] [FILE ...]\n", argv[0]);
+        exit(1);
+    }
   }
-  long nlines = atol(argv[1]);
-  if (argc == 2) {
+
+  if (optind == argc) {
     do_head(stdin, nlines);
   } else {
-    for (int i = 2; i < argc; i++) {
+    for (int i = optind; i < argc; i++) {
       FILE *f;
       f = fopen(argv[i], "r");
       if (!f) {
