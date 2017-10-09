4dc4610581675bb4c8b82881d6417f1c9907cd38
xorphitus <xorphitus@gmail.com>
2017-10-09 23:45:13 +0900

    WIP grepに-iオプション作成

--- 08-03-1_grep.c
+++ 08-03-1_grep.c
@@ -4,6 +4,9 @@
 #include <sys/types.h>
 #include <regex.h>
 
+#define _GNU_SOURCE
+#include <getopt.h>
+
 static void do_grep(regex_t *pat, FILE *f);
 
 int main(int argc, char *argv[]) {
@@ -12,8 +15,25 @@ int main(int argc, char *argv[]) {
     exit(1);
   }
 
+  int reg_flag = REG_EXTENDED | REG_NOSUB | REG_NEWLINE;
+
+  int opt;
+  while ((opt = getopt(argc, argv, "iv")) != -1) {
+    switch (opt) {
+      case 'i':
+        reg_flag = reg_flag | REG_ICASE;
+        break;
+      case 'v':
+        /* TODO */
+        break;
+      case '?':
+        fprintf(stdout, "Unknown option");
+        exit(1);
+    }
+  }
+
   regex_t pat;
-  int err = regcomp(&pat, argv[1], REG_EXTENDED | REG_NOSUB | REG_NEWLINE);
+  int err = regcomp(&pat, argv[1], reg_flag);
   if (err != 0) {
     char buf[1024];
 
@@ -22,10 +42,10 @@ int main(int argc, char *argv[]) {
     exit(1);
   }
 
-  if (argc == 2) {
+  if (argc == 1 + optind) {
     do_grep(&pat, stdin);
   } else {
-    for (int i = 2; i < argc; i++) {
+    for (int i = 1 + optind; i < argc; i++) {
       FILE *f;
 
       f = fopen(argv[i], "r");
