de52fb0986d56c81fd62aa51a254de55ed72dede
xorphitus <xorphitus@gmail.com>
2017-10-11 18:21:29 +0900

    問題10.11 1をざっくり書いてみた(動作未確認)

---
+++ 10-11-1_traverse.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <string.h>
+
+static void traverse(char *path);
+static int is_traversable(char *path);
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    fprintf(stderr, "%s: invalid arguments", argv[0]);
+    exit(1);
+  }
+
+  for (int i = 1; i < argc; i++) {
+    traverse(argv[i]);
+  }
+
+  exit(0);
+}
+
+static void traverse(char *path) {
+  DIR *d = opendir(path);
+  if (!d) {
+    perror(path);
+    exit(1);
+  }
+
+  struct dirent *ent;
+  while ((ent = readdir(d))) {
+    char *name = ent->d_name;
+    printf("%s", name);
+
+    if (strcmp(name, ".") == 0 || strcmp(name, "..") == 0 || is_traversable(name)) {
+      traverse(name);
+    }
+  }
+
+  closedir(d);
+}
+
+static int is_traversable(char *path) {
+  struct stat st;
+  if (lstat(path, &st) < 0) {
+    perror(path);
+    exit(1);
+  }
+
+  return !S_ISLNK(st.st_mode) && S_ISDIR(st.st_mode);
+}
