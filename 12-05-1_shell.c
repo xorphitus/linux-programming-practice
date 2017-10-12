28376087e607c9765627cbdd420ad6030982e88e
xorphitus <xorphitus@gmail.com>
2017-10-12 19:26:45 +0900

    12.5 いくらでもコマンドが打てるようにした

--- 12-05-1_shell.c
+++ 12-05-1_shell.c
@@ -10,32 +10,35 @@
 static void split(char *str, char *results[]);
 
 int main(int argc, char *argv[]) {
-  printf("myshell$ ");
-  char input[100];
-  // TODO: fgetsに変更
-  gets(input);
+  while (1) {
+    printf("myshell$ ");
+    char input[100];
+    // TODO: fgetsに変更
+    gets(input);
 
-  char *command[MAX_ARGS] = {};
-  split(input, command);
+    char *command[MAX_ARGS] = {};
+    split(input, command);
 
-  pid_t pid = fork();
-  if (pid < 0) {
-    fprintf(stderr, "fork(2) failed\n");
-    exit(1);
+    if (strcmp(command[0], "exit") == 0) {
+      exit(0);
+    }
+
+    pid_t pid = fork();
+    if (pid < 0) {
+      fprintf(stderr, "fork(2) failed\n");
+      exit(1);
+    }
+
+    if (pid == 0) {
+      // TODO: オプションと引数を同時に渡すとエラーになる件
+      execvp(command[0], command);
+      perror(input);
+      exit(99);
+    } else {
+      int status;
+      waitpid(pid, &status, 0);
+    }
   }
-
-  if (pid == 0) {
-    // TODO: オプションと引数を同時に渡すとエラーになる件
-    execvp(command[0], command);
-    perror(input);
-    exit(99);
-  } else {
-    int status;
-    waitpid(pid, &status, 0);
-  }
-
-  // TODO: 1コマンドだけで終了しないようにする
-  exit(0);
 }
 
 static void split(char *str, char *results[]) {
