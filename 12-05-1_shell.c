613e17d85b0f735f5831ea178da587ad9534acd2
xorphitus <xorphitus@gmail.com>
2017-10-12 19:50:14 +0900

    12.5 デリミタのスペースが複数あってもOKにした

--- 12-05-1_shell.c
+++ 12-05-1_shell.c
@@ -4,10 +4,12 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <string.h>
+#include <ctype.h>
 
 #define MAX_ARGS 100
 
 static void split(char *str, char *results[]);
+static char* trim(char *str);
 
 int main(int argc, char *argv[]) {
   while (1) {
@@ -47,7 +49,24 @@ static void split(char *str, char *results[]) {
   char *token;
   int i = 1;
   while((token = strtok(NULL, "")) && i < MAX_ARGS) {
+    token = trim(token);
+    if (token[0] == '\0') {
+      continue;
+    }
     results[i] = token;
     i++;
   }
+}
+
+static char* trim(char *str) {
+  if (str && *str) {
+    size_t len = strlen(str);
+    for (size_t i = 0; i < len; i++) {
+      if (isspace(str[i])) {
+        str[i] = '\0';
+      }
+    }
+  }
+
+  return str;
 }
