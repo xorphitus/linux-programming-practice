9206f57fadf91e7a63436aad8a8d8750361be2a3
xorphitus <xorphitus@gmail.com>
2017-10-12 19:08:55 +0900

    12.5 コマンドに引数を渡せるようにした

--- 12-05-1_shell.c
+++ 12-05-1_shell.c
@@ -3,12 +3,20 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <string.h>
+
+#define MAX_ARGS 100
+
+static void split(char *str, char *results[]);
 
 int main(int argc, char *argv[]) {
   printf("myshell$ ");
   char input[100];
   // TODO: fgetsに変更
   gets(input);
+
+  char *command[MAX_ARGS] = {};
+  split(input, command);
 
   pid_t pid = fork();
   if (pid < 0) {
@@ -17,8 +25,8 @@ int main(int argc, char *argv[]) {
   }
 
   if (pid == 0) {
-    // TODO: 引数を渡せるようにする
-    execlp(input, input, NULL);
+    // TODO: オプションと引数を同時に渡すとエラーになる件
+    execvp(command[0], command);
     perror(input);
     exit(99);
   } else {
@@ -28,4 +36,15 @@ int main(int argc, char *argv[]) {
 
   // TODO: 1コマンドだけで終了しないようにする
   exit(0);
+}
+
+static void split(char *str, char *results[]) {
+  results[0] = strtok(str, " ");
+
+  char *token;
+  int i = 1;
+  while((token = strtok(NULL, "")) && i < MAX_ARGS) {
+    results[i] = token;
+    i++;
+  }
 }
