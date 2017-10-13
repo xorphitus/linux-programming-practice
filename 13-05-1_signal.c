3871de2c7bedd9ad1802eee082f78b42ff212209
xorphitus <xorphitus@gmail.com>
2017-10-13 14:01:42 +0900

    13.5完成

--- 13-05-1_signal.c
+++ 13-05-1_signal.c
@@ -1,18 +1,31 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <signal.h>
+
+static void sighandler(int n);
 
 int main(int argc, char *argv[]) {
   printf("waiting SIGINT...\n");
 
-  if (pause() == -1) {
+  struct sigaction act;
+  act.sa_handler = sighandler;
+  sigemptyset(&act.sa_mask);
+  if (sigaction(SIGINT, &act, NULL) < 0) {
     perror(argv[0]);
     exit(1);
-  } else {
-    printf("SIGINT is trapped!\n");
-    exit(0);
+  }
+
+  if (pause() < 0) {
+    perror(argv[0]);
+    exit(1);
   }
 
   fprintf(stderr, "%s is terminated without SIGINT\n", argv[0]);
   exit(1);
+}
+
+static void sighandler(int n) {
+  printf("SIGINT is trapped!\n");
+  exit(0);
 }
